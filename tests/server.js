0264072dc3e753800e37060661670112c264e5ac
Zaky Maulana Al Bajili <87513164+zakialbajili@users.noreply.github.com>
2022-11-07 22:59:06 +0700

    Add Unit Testing

--- tests/server.js
+++ tests/server.js
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+const express = require('express')
+const cors=require('cors')
+//const m$user=require('./modules/user.module')
+//const prisma = require('./helpers/database')
+const routes = require('../routes')
+const app = express()
+const port = 8000
+
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+routes(app)
+
+
+module.exports=app
+=======
 const express = require("express");
 const cors = require("cors");
 const routes = require("/routes");
@@ -15,3 +33,4 @@ routes(app);
 
 
 module.exports = app;
+>>>>>>> 0bcf42ab825d25901377cc293d46dd5bb1f6b957
